e5440a87bd44d2a1d9f64d6a7c4c9704c9f0c79e
Maggie Amin <maggiemmagdy@gmail>
2022-12-20 13:12:30 +0000

    Adding new.html to bookings and modified booking_controller

--- controllers/booking_controller.py
+++ controllers/booking_controller.py
@@ -11,3 +11,18 @@ bookings_blueprint = Blueprint("bookings", __name__)
 def bookings():
     bookings = booking_repository.select_all()
     return render_template("bookings/index.html", bookings=bookings)
+
+@bookings_blueprint.route("/bookings/new")
+def new_booking():
+    members = member_repository.select_all()
+    gym_classes = gym_class_repository.select_all()
+    return render_template("bookings/new.html", members=members, gym_classes=gym_classes)
+
+@bookings_blueprint.route("/bookings", methods=['POST'])
+def create_booking():
+    print(request.form)
+    member = member_repository.select(request.form["member_id"])
+    gym_class = member_repository.select(request.form["gym_class_id"])
+    booking = Booking(member, gym_class)
+    booking_repository.save(booking)
+    return redirect("/bookings")
